606f83cbaea4c48a194d61969b43c75bcd5491d2
Vivek Karthick S <vivek.k@blue.cloud>
2024-08-13 16:05:01 +0530

    stop updre

--- demo.py
+++ demo.py
@@ -1,3 +1,32 @@
 # This program prints Hello, world!
 for j in range(1,11):
     print('Hello, world ! - '+str(j))
+import boto3
+
+client = boto3.client('ec2', region_name='us-east-1')
+
+instance_tags = client.describe_instances(
+    Filters=[
+        {
+            'Name': 'tag:Environment',
+            'Values': [
+                'Dev',
+            ]
+        },
+        {
+            'Name': 'instance-state-name', 
+            'Values': [
+                'running',
+            ]
+        }    
+    ],
+)
+
+ids= [instance['InstanceId']
+    for reservation in instance_tags['Reservations']
+    for instance in reservation['Instances']]
+    
+response = client.stop_instances(
+        InstanceIds=ids
+)
+print("Stopped now", response)
